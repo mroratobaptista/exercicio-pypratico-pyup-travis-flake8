164b67e6c335f8dddcbdeba07ff707996510ba74
Matheus Rorato Baptista <matheusrorato@gmail.com>
2017-05-21 15:31:39 -0300

    arrumando pep8

--- setup.py
+++ setup.py
@@ -130,7 +130,8 @@ setup(
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License "
+        "v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Framework :: Paste",
